c40cb535f13d1a63e3fed39d755a748c097413ee
CodrutaA <42152594+CodrutaA@users.noreply.github.com>
2021-01-06 12:47:15 +0200

    Update RunCucumberTest.java

--- src/test/java/FirstProjectBDD/RunCucumberTest.java
+++ src/test/java/FirstProjectBDD/RunCucumberTest.java
@@ -5,6 +5,8 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
+        glue = "",
+        features = "",
         plugin = {"pretty"})
 
 public class RunCucumberTest {
